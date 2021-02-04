78a903328f15beb86fd33caf14f1423f2a716212
Marina Montag <montag.marinka@gmail.com>
2021-02-04 14:56:20 +0200

    made Spinner class

---
+++ Lab/src/main/java/by/marina/labA/Spinner.java
@@ -0,0 +1,20 @@
+package by.marina.labA;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+
+public class Spinner{
+   private JSpinner spinner;
+   private  MyThread thread;
+    Spinner(int y, MyThread thread){
+        SpinnerModel value=new SpinnerNumberModel(1,1,10,1);
+       spinner=new JSpinner(value);
+       this.thread=thread;
+       spinner.setBounds(200,y,30,30);
+       spinner.addChangeListener(this::changePriority);
+    }
+
+    private void changePriority(ChangeEvent e){
+        thread.setPriority((Integer) ((JSpinner)e.getSource()).getValue());
+    }
+}
